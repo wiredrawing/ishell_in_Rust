1c7dbfb1ed97e8aa0059fa3be30692ddb3139546
senbiki akifumi <akifumi.senbiki.1209@gmail.com>
2020-06-28 00:12:20 +0900

    ファイルの内容を削除する方法をset_lenメソッドからファイルを作成し直す方法に変更

--- src/main.rs
+++ src/main.rs
@@ -46,10 +46,6 @@ use echo::*;
 
 fn main() {
 
-    let function = | param: String | {
-        echo (&param);
-    };
-
     let default_command : String = "php".to_string();
     // 実行時のコマンドライン引数を取得
     let arguments: Vec<String> = env::args().collect();
@@ -81,29 +77,20 @@ fn main() {
     let mut validate_dir : PathBuf = env::temp_dir();
     validate_dir.push("validate_log.dat");
     let validate_file_path = format!("{}", validate_dir.display());
-    let validate_file : Result<fs::File, Error> = fs::File::create(&validate_file_path);
+
+    // ファイル作成関数を利用する
+    let mut validate_file : std::fs::File = create_new_file(&validate_file_path);
+
 
     // 実行用
     let mut execute_dir: PathBuf = env::temp_dir();
     execute_dir.push("execute_log.dat");
     let execute_file_path = format!("{}", execute_dir.display());
-    let execute_file : Result<fs::File, Error> = fs::File::create(&execute_file_path);
-
-    // 検証用ソース・ファイルの作成失敗時
-    if (validate_file.is_ok() != true) {
-        panic!("{}", validate_file.unwrap_err());
-        process::exit(my_pid as i32);
-    }
-    let mut validate_file = validate_file.unwrap();
+    // ファイル作成関数を利用する
+    let mut execute_file : std::fs::File = create_new_file( &execute_file_path);
     // 検証用ファイルの初期化
     validate_file.write_all(initialize_input.as_bytes());
 
-    // 実行用ソース・ファイルの作成失敗時
-    if (execute_file.is_ok() != true) {
-        panic!("{}", execute_file.unwrap_err().to_string());
-        process::exit(my_pid as i32);
-    }
-    let mut execute_file = execute_file.unwrap();
 
 
     let running = Arc::new(AtomicBool::new(true));
@@ -145,14 +132,27 @@ fn main() {
         } else if (CLEAR_STRING.to_string() == input ) {
             // 正常実行が完了していた直近のソースコードまで巻き上げる
             let backup_string : String = fs::read_to_string(&execute_file_path).unwrap();
-            validate_file.set_len(0);
+            // 検証用ファイルを一旦削除
+            remove_target_file(&validate_file_path);
+            // 再度検証用ファイルを作成
+            validate_file = create_new_file(&validate_file_path);
             validate_file.write_all(backup_string.as_bytes());
             input.clear();
             continue;
         } else if (DEL_STRING.to_string() == input) {
-            validate_file.set_len(0);
+            // 検証用ファイルを削除
+            if (remove_target_file(&validate_file_path) != true)  {
+                panic!("Failed to remove the file named {} which you selected .");
+            }
+            // 実行用ファイルを削除
+            if (remove_target_file(&validate_file_path) != true)  {
+                panic!("Failed to remove the file named {} which you selected .");
+            }
+            // 再度検証用ファイルを作成する
+            validate_file = create_new_file(&validate_file_path);
             validate_file.write_all("<?php \r\n".as_bytes()).unwrap();
-            execute_file.set_len(0);
+
+            execute_file = create_new_file(&execute_file_path);
             execute_file.write_all("<?php \r\n".as_bytes()).unwrap();
             input.clear();
             continue;
@@ -196,17 +196,19 @@ fn main() {
 
             // 検証用ファイルでプログラムが正常終了した場合
             let mut temp_file : String = fs::read_to_string(&validate_file_path).unwrap();
-            execute_file.set_len(0);
 
             // プログラムが正常終了している場合のみ改行出力を追加
             temp_file.push_str("\nprint(\"\n\"); \n");
+            remove_target_file(&execute_file_path);
+            execute_file = create_new_file(&execute_file_path);
             written_bytes = execute_file.write_all(temp_file.as_bytes());
             if (written_bytes.is_ok() != true) {
                 panic!("Err: {}", written_bytes.unwrap_err().to_string());
             }
 
             // 検証用ファイルを再度削除し、改行出力を追加した分を再度保存
-            validate_file.set_len(0);
+            remove_target_file(&validate_file_path);
+            validate_file = create_new_file(&validate_file_path);
             written_bytes = validate_file.write_all(temp_file.as_bytes());
 
             // 実行用ファイルで再度コマンド実行
@@ -250,8 +252,38 @@ fn main() {
 }
 
 
-// テキストに任意の文字列が含まれているかどうか
-// 含まれない場合 -1の負の数を返却する
+/// 引数に渡した、ファイルパスで新規ファイルを作成する
+/// ファイルの作成に失敗した場合は、パニックをおこす
+fn create_new_file (path : &String ) -> std::fs::File
+{
+    let new_file : Result <std::fs::File, Error> = fs::File::create(path);
+
+    if (new_file.is_ok() != true) {
+        // パニックでプロセスを落とす
+        println!("Err {}", new_file.unwrap_err());
+        panic!("Failed new file named {}", path);
+    }
+    return new_file.unwrap();
+}
+
+
+/// 引数に渡したファイルパスが存在していれば、削除
+/// しなければ、パニックを起こす。
+fn remove_target_file (path : &String) -> bool
+{
+    use std::path::Path;
+    // Pathオブジェクトを作成
+    let target_path : &Path = Path :: new (path);
+
+    // ファイルが存在しない場合、panicを起こす
+    if (target_path.exists() != true) {
+        panic!("Failed to remove the file named {} which you selected.", path);
+    }
+    return true;
+}
+
+/// テキストに任意の文字列が含まれているかどうか
+/// 含まれない場合 -1の負の数を返却する
 fn str_position (target : &String, needle : u8) -> i32
 {
     check_type(target.as_bytes().iter().enumerate());
@@ -264,7 +296,7 @@ fn str_position (target : &String, needle : u8) -> i32
 }
 
 
-// 末尾の改行文字を削除
+/// 末尾の改行文字を削除
 fn remove_newline(newline_string : &mut String)
 {
     if (newline_string.len() == 0) {
@@ -295,6 +327,7 @@ fn remove_newline(newline_string : &mut String)
     }
 }
 
+/// オブジェクトの型チェック
 fn check_type<T>(_: T) -> String {
     // println!("{}", std::any::type_name::<T>());
     return std::any::type_name::<T>().to_string();
